fdb2a03809976d8e8f286b7a2d827ddb74a31d7b
michaelhalim <mikefla10@gmail.com>
2017-02-05 20:39:02 +0700

    Layout

--- app/src/main/java/com/gghouse/woi/whatsonininput/MainActivity.java
+++ app/src/main/java/com/gghouse/woi/whatsonininput/MainActivity.java
@@ -2,12 +2,17 @@ package com.gghouse.woi.whatsonininput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mETDaerah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mETDaerah = (EditText) findViewById(R.id.et_AM_daerah);
     }
 }
