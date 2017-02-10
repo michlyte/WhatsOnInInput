2731d002c8751e2cfcd7614841c2496c7305e6e5
michaelhalim <mikefla10@gmail.com>
2017-02-10 22:35:19 +0700

    AddActivity

--- app/src/main/java/com/gghouse/woi/whatsonininput/AddActivity.java
+++ app/src/main/java/com/gghouse/woi/whatsonininput/AddActivity.java
@@ -11,7 +11,7 @@ public class AddActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add);
 
         mETDaerah = (EditText) findViewById(R.id.et_AM_daerah);
     }
