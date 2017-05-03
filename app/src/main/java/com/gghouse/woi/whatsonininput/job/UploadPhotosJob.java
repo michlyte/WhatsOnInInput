ca4fd2a70b6b06655f14e80b9ac91adc605de1e5
Michlyte <mikefla10@gmail.com>
2017-05-03 16:13:56 +0700

    Upload compression

--- app/src/main/java/com/gghouse/woi/whatsonininput/job/UploadPhotosJob.java
+++ app/src/main/java/com/gghouse/woi/whatsonininput/job/UploadPhotosJob.java
@@ -1,13 +1,10 @@
 package com.gghouse.woi.whatsonininput.job;
 
-import android.graphics.Bitmap;
-
 import com.gghouse.woi.whatsonininput.WOIInputApplication;
 import com.gghouse.woi.whatsonininput.common.Config;
 import com.gghouse.woi.whatsonininput.model.StoreFileLocation;
 import com.gghouse.woi.whatsonininput.util.ImageHelper;
 import com.gghouse.woi.whatsonininput.util.Logger;
-import com.gghouse.woi.whatsonininput.util.Session;
 import com.gghouse.woi.whatsonininput.webservices.ApiClient;
 import com.gghouse.woi.whatsonininput.webservices.response.StoreUploadPhotosResponse;
 import com.path.android.jobqueue.Job;
@@ -38,24 +35,17 @@ public class UploadPhotosJob extends Job {
 
     @Override
     public void onRun() throws Throwable {
-        StoreFileLocation updatedStoreFileLocation = mStoreFileLocation;
-
-        Bitmap bitmap = ImageHelper.loadImageFromStorage(mStoreFileLocation.getLocation());
-        String bitmapBase64 = "";
-        if (bitmap != null) {
-            bitmapBase64 = ImageHelper.convertBitmapToString(bitmap);
-            updatedStoreFileLocation.setStrImgBase64(bitmapBase64);
-        }
-
-        if (!bitmapBase64.isEmpty()) {
+        String bitmapBase64 = ImageHelper.getStringFromPath(mStoreFileLocation.getLocation());
+        if (bitmapBase64 != null) {
+            mStoreFileLocation.setStrImgBase64(bitmapBase64);
             List<StoreFileLocation> storeFileLocationList = new ArrayList<StoreFileLocation>();
-            storeFileLocationList.add(updatedStoreFileLocation);
+            storeFileLocationList.add(mStoreFileLocation);
 
             Call<StoreUploadPhotosResponse> callUploadPhotos = ApiClient.getClient().uploadPhotos(storeFileLocationList);
             StoreUploadPhotosResponse storeUploadPhotosResponse = callUploadPhotos.execute().body();
             switch (storeUploadPhotosResponse.getCode()) {
                 case Config.CODE_200:
-                    Session.removeLocalPhoto(WOIInputApplication.getAppContext(), mStoreFileLocation);
+//                    Session.removeLocalPhoto(WOIInputApplication.getAppContext(), mStoreFileLocation);
                     if (WOIInputApplication.getInstance().getJobManager().count() == 0) {
                         Logger.log("This is the last.");
                     }
--- app/src/main/java/com/gghouse/woi/whatsonininput/util/ImageHelper.java
+++ app/src/main/java/com/gghouse/woi/whatsonininput/util/ImageHelper.java
@@ -5,30 +5,23 @@ import android.graphics.BitmapFactory;
 import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 /**
  * Created by michaelhalim on 5/2/17.
  */
 
 public abstract class ImageHelper {
-    public static Bitmap loadImageFromStorage(String path) {
-        try {
-            File f = new File(path);
-            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
-            return b;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    public static String getStringFromPath(String path) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inSampleSize = 2;  //you can also calculate your inSampleSize
+        options.inJustDecodeBounds = false;
+        options.inTempStorage = new byte[16 * 1024];
 
-    public static String convertBitmapToString(Bitmap bitmap) {
+        Bitmap bitmap = BitmapFactory.decodeFile(path, options);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-
+        bitmap.recycle();
         return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
     }
 }
